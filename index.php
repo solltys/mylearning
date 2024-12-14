6043ef882093802ea77f9a00e719a7c7ea09c6cc
solltys <114074623+solltys@users.noreply.github.com>
2024-12-14 13:57:27 +0100

    First Commit update

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Learning PHP Page</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <div class="main">
+            <h1>The First Page</h1>
+            <h2>Shopping Cart</h2>
+            <p>Items</p>
+            <p>Cost per pack</p>
+            <p>Subtotal:</p>
+            <p>Tax:</p>
+            <p>Total:</p>
+        </div>
+    </body>
+</html>
