d32aee4d6adaa28c5c11b5b6e48d06b3ed36e3ce
solltys <114074623+solltys@users.noreply.github.com>
2024-12-14 18:12:51 +0100

    First changes

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        <meta charset="utf-8">
+        <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>My Learning PHP Page</title>
         <link rel="stylesheet" href="css/styles.css">
+        <title>Mr. Soltys Projects</title>
     </head>
     <body>
-        <div class="main">
+        <nav>
+            <!-- Nav bar to display logo, Title, Info, Link to Mr. Soltys Page, Blog and Contact -->
+        </nav>
+        <menu>
+            <!-- side bar manu to display main chapters of the project-->
+        </menu>
+        <main><div class="main">
             <h1>The First Page</h1>
             <h2>Shopping Cart</h2>
             <p>Items</p>
@@ -15,6 +21,6 @@
             <p>Subtotal:</p>
             <p>Tax:</p>
             <p>Total:</p>
-        </div>
+        </main></div>
     </body>
 </html>
