ca7c893c20c85d59b4d6675511d2ac2b7ecd72b0
solltys <114074623+solltys@users.noreply.github.com>
2025-01-18 21:08:31 +0100

    First Version with categories

--- index.php
+++ index.php
@@ -4,23 +4,55 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/styles.css">
-        <title>Mr. Soltys Projects</title>
+        <title>Mr. Soltys Projects</title> 
+        <link rel="icon" href="/Assets/Logo.png">
     </head>
+   
     <body>
         <nav>
-            <!-- Nav bar to display logo, Title, Info, Link to Mr. Soltys Page, Blog and Contact -->
+           <div class="navbar">
+            <a href="index.php"><img id="logo" src="/Assets/Logo.png" alt="My Logo Image"></a>
+            <header>Mr. Soltys Learning Page</header>
+            <ul>
+                <li><a href="#">PHP Text Book</a></li>
+                <li><a href="">Projects</a></li>
+            </ul>
+            </div>
         </nav>
-        <menu>
-            <!-- side bar manu to display main chapters of the project-->
-        </menu>
-        <main><div class="main">
-            <h1>The First Page</h1>
-            <h2>Shopping Cart</h2>
-            <p>Items</p>
-            <p>Cost per pack</p>
-            <p>Subtotal:</p>
-            <p>Tax:</p>
-            <p>Total:</p>
-        </main></div>
+        <main> 
+            <menu>
+                    <div class="sidebar">
+                        <h1>PHP & MY SQL Book</h1>
+                        <h2 class="sidebar_heading">Section A: Basic Programming Instructions</h2>
+                        <ul>
+                            <li class="chapter"><a href="#" id="A:Introduction">Introduction</a></li>
+                            <li class="chapter"><a href="#" id="Chapter1">Chapter 1: Variables, Expressions and</br>Operators</a></li>
+                            <li class="chapter"><a href="#" id="Chapter2">Chapter 2: Control Structures</a></li>
+                            <li class="chapter"><a href="#" id="Chapter3">Chapter 3: Functions</a></li>
+                            <li class="chapter"><a href="#" id="Chapter4">Chapter 4: Objects</a></li>
+                        </ul>
+                        <h2 class="sidebar_heading">Section B: Dynamic Web Pages</h2>
+                        <ul>
+                            <li class="chapter"><a href="#" id="B:Introduction">Introduction</a></li>
+                            <li class="chapter"><a href="#" id="Chapter5">Chapter 5: Built-in Functions</a></li>
+                            <li class="chapter"><a href="#" id="Chapter6">Chapter 6: Getting Data From Browsers</a></li>
+                            <li class="chapter"><a href="#" id="Chapter7">Chapter 7: Images and Files</a></li>
+                            <li class="chapter"><a href="#" id="Chapter8">Chapter 8: Dates and Times</a></li>
+                            <li class="chapter"><a href="#" id="Chapter9">Chapter 9: Cookies and Sessions</a></li>
+                            <li class="chapter"><a href="#" id="Chapter10">Chapter 10: Error Handling</a></li>
+                        </ul>
+                    </div>
+            </menu>
+                <div class="main">
+                    <h1>The First Page</h1>
+                    <h2>Shopping Cart</h2>
+                    <p>Items</p>
+                    <p>Cost per pack</p>
+                    <p>Subtotal:</p>
+                    <p>Tax:</p>
+                    <p>Total:</p>
+                </div>
+            
+        </main>
     </body>
 </html>
