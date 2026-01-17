58b181ee5f14125d84f106e9ef6f8fc1f23498cf
solltys <114074623+solltys@users.noreply.github.com>
2026-01-17 14:40:30 +0100

    Finished at page 165 - using getters and setters

--- Chapters/chapter4.php
+++ Chapters/chapter4.php
@@ -1,1 +1,113 @@
-<?phpdeclare ( strict_types = 1 );class Customer {    public string $firstname;    public string $surname;    public string $email;    public string $password;}class Account {    public int $number;    public string $type;    protected float $balance;        public function __construct(int $number, string $type, float $balance = 0.00) {        $this->number = $number;        $this->type = $type;        $this->balance = $balance;    }        public function deposit(float $amount): float {        $this->balance +=$amount;        return $this->balance;    }        public function withdrawal(float $amount):float {        $this->balance -=$amount;        return $this->balance;    }        public function getBalance() {        return $this->balance;    }}$savings = new Account(12546222, 'Savings', 852.00);$current = new Account(256985, 'Current', 85.00);$investment = new Account(25879666,'Investment', 25000.00);?><!DOCTYPE html><?php require_once '../includes/chapters_header.php'; ?><body><?php require_once '../includes/chapters_navbar.php'; ?><main>    <menu>        <div class="sidebar">            <h1>PHP & MY SQL Book</h1>            <h2 class="sidebar_heading">Section A: Basic Programming Instructions</h2>            <ul>                <li class="chapter"><a href="introduction.php" id="A:Introduction">Introduction</a></li>                <li class="chapter"><a href="chapter1.php" id="Chapter1">Chapter 1: Variables, Expressions and</br>                    Operators</a></li>                <li class="chapter"><a href="chapter2.php" id="Chapter2">Chapter 2: Control Structures</a></li>                <li class="chapter"><a href="chapter3.php" id="Chapter3">Chapter 3: Functions</a></li>                <li class="chapter selected"><a href="chapter4.php" id="Chapter4">Chapter 4: Objects</a></li>            </ul>            <h2 class="sidebar_heading">Section B: Dynamic Web Pages</h2>            <ul>                <li class="chapter"><a href="#" id="B:Introduction">Introduction</a></li>                <li class="chapter"><a href="#" id="Chapter5">Chapter 5: Built-in Functions</a></li>                <li class="chapter"><a href="#" id="Chapter6">Chapter 6: Getting Data From Browsers</a></li>                <li class="chapter"><a href="#" id="Chapter7">Chapter 7: Images and Files</a></li>                <li class="chapter"><a href="#" id="Chapter8">Chapter 8: Dates and Times</a></li>                <li class="chapter"><a href="#" id="Chapter9">Chapter 9: Cookies and Sessions</a></li>                <li class="chapter"><a href="#" id="Chapter10">Chapter 10: Error Handling</a></li>            </ul>        </div>    </menu>    <div class="main chapter4">        <img src="../img/bank_logo.png" alt="bank_logo" height="30" id="bank_logo" title="BankLogo">                    <table>        <tr>            <th>Date</th>            <th><?= $current->type ?></th>            <th><?= $savings->type ?></th>        </tr>        <tr>            <td>23 June</td>            <td><?= $current->getBalance() ?></td>            <td><?= $savings->getBalance() ?></td>        </tr>        <tr>            <td>24 June</td>            <td><?= $current->deposit(26.00) ?></td>            <td><?= $savings->withdrawal(250.00) ?></td>        </tr>        <tr>            <td>25 June</td>            <td><?= $current->withdrawal(6.00) ?></td>            <td><?= $savings->deposit(100.00) ?></td>        </tr>        </table>        <h2><?= $investment->type ?> Account</h2>        <p>Previous Balance € <?= $investment->getBalance() ?></p>        <p>New Balance €<?= $investment->deposit(2500.00) ?></p>    </div></main><?php include '../includes/footer.php'; ?></body></html>
+<?php
+declare ( strict_types = 1 );
+
+class Customer {
+    public string $firstname;
+    public string $surname;
+    public string $email;
+    public string $password;
+}
+
+class Account 
+{
+    public int $number;
+    public string $type;
+    protected float $balance;
+    
+    public function __construct(int $number, string $type, float $balance = 0.00) {
+        $this->number = $number;
+        $this->type = $type;
+        $this->balance = $balance;
+    }
+    
+    public function deposit(float $amount): float {
+        $this->balance +=$amount;
+        return $amount;
+    }
+    
+    public function withdrawal(float $amount):float {
+        $this->balance -=$amount;
+        return $amount;
+    }
+    
+    public function getBalance():float {
+        return $this->balance;
+    }
+
+    public function setBalance(float $balance):void {
+        $this->balance = $balance;
+}
+}
+
+$savings = new Account(12546222, 'Savings', 852.00);
+$current = new Account(256985, 'Current', 85.00);
+$investment = new Account(25879666,'Investment', 25000.00);
+
+
+?>
+
+<!DOCTYPE html>
+<?php require_once '../includes/chapters_header.php'; ?>
+<body>
+<?php require_once '../includes/chapters_navbar.php'; ?>
+<main>
+    <menu>
+        <div class="sidebar">
+            <h1>PHP & MY SQL Book</h1>
+            <h2 class="sidebar_heading">Section A: Basic Programming Instructions</h2>
+            <ul>
+                <li class="chapter"><a href="introduction.php" id="A:Introduction">Introduction</a></li>
+                <li class="chapter"><a href="chapter1.php" id="Chapter1">Chapter 1: Variables, Expressions and</br>
+                    Operators</a></li>
+                <li class="chapter"><a href="chapter2.php" id="Chapter2">Chapter 2: Control Structures</a></li>
+                <li class="chapter"><a href="chapter3.php" id="Chapter3">Chapter 3: Functions</a></li>
+                <li class="chapter selected"><a href="chapter4.php" id="Chapter4">Chapter 4: Objects</a></li>
+            </ul>
+            <h2 class="sidebar_heading">Section B: Dynamic Web Pages</h2>
+            <ul>
+                <li class="chapter"><a href="#" id="B:Introduction">Introduction</a></li>
+                <li class="chapter"><a href="#" id="Chapter5">Chapter 5: Built-in Functions</a></li>
+                <li class="chapter"><a href="#" id="Chapter6">Chapter 6: Getting Data From Browsers</a></li>
+                <li class="chapter"><a href="#" id="Chapter7">Chapter 7: Images and Files</a></li>
+                <li class="chapter"><a href="#" id="Chapter8">Chapter 8: Dates and Times</a></li>
+                <li class="chapter"><a href="#" id="Chapter9">Chapter 9: Cookies and Sessions</a></li>
+                <li class="chapter"><a href="#" id="Chapter10">Chapter 10: Error Handling</a></li>
+            </ul>
+        </div>
+    </menu>
+    <div class="main chapter4">
+        <img src="../img/bank_logo.png" alt="bank_logo" height="30" id="bank_logo" title="BankLogo">
+    
+        
+        <table>
+        <tr>
+            <th>Date</th>
+            <th><?= $current->type ?></th>
+            <th><?= $savings->type ?></th>
+        </tr>
+        <tr>
+            <td>23 June</td>
+            <td><?= $current->getBalance() ?></td>
+            <td><?= $savings->getBalance() ?></td>
+        </tr>
+        <tr>
+            <td>24 June</td>
+            <td><?= $current->deposit(26.00) ?></td>
+            <td><?= $savings->withdrawal(250.00) ?></td>
+        </tr>
+        <tr>
+            <td>25 June</td>
+            <td><?= $current->withdrawal(6.00) ?></td>
+            <td><?= $savings->deposit(100.00) ?></td>
+        </tr>
+        </table>
+        <h2><?= $investment->type ?> Account</h2>
+        <p>Previous Balance € <?= $investment->getBalance() ?></p>
+        <p>Withdrawal €<?= $investment->withdrawal(50.00) ?></p>
+        <p>Deposit €<?= $investment->deposit(2500.00) ?></p>
+        <p>New Balance €<?= $investment->getBalance() ?></p>
+    </div>
+</main>
+<?php include '../includes/footer.php'; ?>
+</body>
+</html>
